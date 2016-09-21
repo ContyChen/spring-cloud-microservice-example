cddf8ec6f1bf4828c315aee2213149136c5ffe10
yingzhuo <yingzhor@gmail.com>
2016-09-21 11:41:21 +0800

    update

--- api-gateway/src/main/java/example/api/gateway/filter/SecurityZuulFilterRegistrationBean.java
+++ api-gateway/src/main/java/example/api/gateway/filter/SecurityZuulFilterRegistrationBean.java
@@ -3,10 +3,12 @@ package example.api.gateway.filter;
 import com.netflix.zuul.filters.FilterRegistry;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("security")
 public class SecurityZuulFilterRegistrationBean implements ApplicationRunner, Ordered {
 
     @Override
